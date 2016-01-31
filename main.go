0df2fe4c074427bf2c2ff7069f04d540323f44c5
shaybix <shaybix@tuta.io>
2016-01-31 03:23:31 +0000

    created gitignore file

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// Basic http server listening on port 8000
+
 func main() {
-	//
+	fmt.Println("Listening on local port 8000")
+
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatalln(err)
+
+	}
+
 }
