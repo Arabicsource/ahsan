75e686e2c824c383f8ed40d0689ff769fb9dc0af
shaybix <shaybix@tuta.io>
2016-02-27 23:20:26 +0000

    updates

--- main.go
+++ main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
+
+var interval = flag.Float64("interval", 12, "Default 12 Hours")
+
+type Crawler struct{}
 
 // Book to be downloaded from the shamela website
 type Book struct {
 	Bid  int
 	Name string
-	Page string
+	Link string
 	Bok  string
 	Pdf  string
 }
@@ -26,12 +32,35 @@ type Link struct {
 
 // Basic http server listening on port 8000
 func main() {
-	fmt.Println("Listening on local port 8000")
 
-	err := http.ListenAndServe(":8000", nil)
+	c := new(Crawler)
+
+	file, err := os.Create("urls.json")
+
+	if err != nil {
+		log.Println(err)
+
+	}
+
+	go c.run(file)
+	fmt.Println("Listening on local port 8000")
+	err = http.ListenAndServe(":8000", c)
 	if err != nil {
 		log.Fatalln(err)
 
 	}
 
+}
+
+func (c *Crawler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	// handle any incoming requests
+
+}
+
+func (c *Crawler) run(file *os.File) {
+
+	fileinfo  := file.Stat()
+	fileinfo.
+	// a for loop
 }
