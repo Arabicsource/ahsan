fc0dc7a8fbffaf031c8b68ae9be61127cec291c0
shaybix <shaybix@tuta.io>
2016-02-28 21:05:37 +0000

    took out much of the code specific to the crawler

--- main.go
+++ main.go
@@ -1,22 +1,14 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-)
+import "flag"
 
 var interval = flag.Float64("interval", 12, "Default 12 Hours")
-
-type Crawler struct{}
 
 // Book to be downloaded from the shamela website
 type Book struct {
 	Bid  int
 	Name string
-	Link string
+	Link Link
 	Bok  string
 	Pdf  string
 }
@@ -32,35 +24,16 @@ type Link struct {
 
 // Basic http server listening on port 8000
 func main() {
-
+	// Create new Crawler
 	c := new(Crawler)
 
-	file, err := os.Create("urls.json")
+	c.run()
 
-	if err != nil {
-		log.Println(err)
+	//	fmt.Println("Listening on local port 8000")
+	//	err := http.ListenAndServe(":8000", c)
+	//	if err != nil {
+	//		log.Fatalln(err)
+	//
+	//	}
 
-	}
-
-	go c.run(file)
-	fmt.Println("Listening on local port 8000")
-	err = http.ListenAndServe(":8000", c)
-	if err != nil {
-		log.Fatalln(err)
-
-	}
-
-}
-
-func (c *Crawler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
-	// handle any incoming requests
-
-}
-
-func (c *Crawler) run(file *os.File) {
-
-	fileinfo  := file.Stat()
-	fileinfo.
-	// a for loop
 }
