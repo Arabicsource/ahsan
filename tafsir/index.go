ce22044f36e1c6a8cbbe0e1e6af7daf33c1a4499
shaybix <shaybix@tuta.io>
2016-06-22 02:15:57 +0100

    bad file descriptor

--- tafsir/index.go
+++ tafsir/index.go
@@ -10,51 +10,87 @@ import (
 	"strings"
 )
 
-func dump(file os.FileInfo) {
-	err := os.Setenv("MDB_JET3_CHARSET", "cp1256")
+func dump(file os.FileInfo) (SQLFile string, err error) {
+	err = os.Setenv("MDB_JET3_CHARSET", "cp1256")
 	if err != nil {
-		log.Println(err)
+		return "", err
 	}
 
 	cmd := exec.Command("mdb-tables", "bok/"+file.Name())
 
 	output, err := cmd.StdoutPipe()
 	if err != nil {
-		log.Println(err)
+		return "", err
 	}
 
 	if err := cmd.Start(); err != nil {
-		log.Println(err)
+		return "", err
 	}
 
 	r := bufio.NewReader(output)
+	output.Close()
 
 	line, _, err := r.ReadLine()
 	if err != nil {
-		log.Println(err)
+		return "", err
 	}
 
 	l := string(line)
 	tables := strings.Fields(l)
-	for _, table := range tables {
-		if strings.HasPrefix(table, "b") || strings.HasPrefix(table, "t") {
 
-			cmd = exec.Command("mdb-export", "-I", "mysql", "bok/"+file.Name(), table)
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-			err := cmd.Run()
-			if err != nil {
-				log.Println(err)
-			}
+	var f *os.File
+	defer f.Close()
 
-		}
+	fn := strings.Split(file.Name(), ".")[0]
+
+	err = os.MkdirAll("sql", 0755)
+	if err != nil {
+		return "", err
 	}
 
+	f, err = os.Create("sql/" + fn + ".sql")
+	if err != nil {
+		return "", err
+	}
+
+	f.Write([]byte("BEGIN;"))
+
+	for _, table := range tables {
+
+		export := exec.Command("mdb-export", "-I", "mysql", "bok/"+file.Name(), table)
+
+		//export.Stdout = os.Stdout
+		//export.Stderr = os.Stderr
+
+		out, err := export.StdoutPipe()
+		if err != nil {
+			return "", err
+		}
+
+		if err = export.Start(); err != nil {
+			return "", err
+		}
+
+		rd := bufio.NewReader(out)
+
+		if err = export.Wait(); err != nil {
+			return "", err
+		}
+
+		_, err = rd.WriteTo(f)
+		if err != nil {
+			return "", err
+		}
+
+		out.Close()
+
+	}
+
+	f.Write([]byte("COMMIT;"))
+	return f.Name(), nil
 }
 
 func main() {
-
-	fmt.Println("initialising the process")
 
 	files, err := ioutil.ReadDir("bok")
 	if err != nil {
@@ -62,7 +98,13 @@ func main() {
 	}
 
 	for _, file := range files {
-		dump(file)
+		SQLFile, err := dump(file)
+		if err != nil {
+			log.Fatal(err)
+
+		}
+		fmt.Printf("Completed SQL file: %v", SQLFile)
+
 	}
 
 }
