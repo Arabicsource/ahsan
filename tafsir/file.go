4167557dfa0d943586b36f374d4d1376a4844827
shaybix <shaybix@tuta.io>
2016-12-02 06:56:22 +0000

    create file.go and readFromFile method

---
+++ tafsir/file.go
@@ -0,0 +1,10 @@
+package main
+
+// URL is a url address of type string
+type URL string
+
+func file() []URL {
+	// TODO: require to parse file flag and store the urls in a slice of type URL
+	var urls []URL
+	return urls
+}
